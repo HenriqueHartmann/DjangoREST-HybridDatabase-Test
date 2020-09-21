db676431750a3da143fbfba7177e7b6011be47a6
Hartmann <alemaohartmann@gmail.com>
2020-09-21 17:07:24 -0300

    :hammer: Refatorar o import do campo JSONField e adicionar max_length ao campo created_at

--- backend/core/models/user.py
+++ backend/core/models/user.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 
 
 class User(models.Model):
     profile = JSONField()
     email = models.EmailField(unique=True)
-    created_at = models.CharField()
+    created_at = models.CharField(max_length=19)
 
     def __str__():
         return self.name
