85689456c48bc968696022e2416b31f4d8144c90
Hartmann <alemaohartmann@gmail.com>
2020-09-21 17:12:31 -0300

    :izakaya_lantern: Add event Serializer

---
+++ backend/core/serializers/event.py
@@ -0,0 +1,38 @@
+from rest_framework_mongoengine import serializers
+
+from backend.core.models import Event, Author
+
+
+class AuthorSerializer(serializers.EmbeddedDocumentSerializer):
+    ref_name = "AuthorEvent"
+    model = Author
+
+
+class EventSerializer(serializers.DocumentSerializer):
+    authors = AuthorSerializer(many=True)
+
+    class Meta:
+        ref_name = "Event"
+        model = Event
+        fields = "__all__"
+
+    def create(self, validated_data):
+        authors_data = validated_data.pop("authors")
+        event = Event.objects.create(**validated_data)
+        event.authors = []
+
+        for author_data in auhtors_data:
+            event.authors.append(Author(**author_data))
+
+        event.save()
+        return event
+
+    def update(self, instance, validated_data):
+        authors_data = validated_data.pop("authors")
+        event = super(EventSerializer, self).update(instance, validated_data)
+
+        for author_data in authors_data:
+            event.authors.append(Author(**author_data))
+
+        event.save()
+        return event
