414ba652997d4190c1c3903385fb99ac822712fe
Hartmann <alemaohartmann@gmail.com>
2020-09-21 17:16:10 -0300

    :roller_coaster: Add rota event

--- backend/core/router.py
+++ backend/core/router.py
@@ -4,3 +4,4 @@ from backend.core import views
 
 router = routers.DefaultRouter()
 router.register("users", views.UserViewSet, basename="user")
+router.register("event", views.EventViewSet, basename="event")
