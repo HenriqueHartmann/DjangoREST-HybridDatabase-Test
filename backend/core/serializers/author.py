2c9d0462a42701c31f994166ac2e11c5fd2ac677
Hartmann <alemaohartmann@gmail.com>
2020-09-22 16:21:53 -0300

    :hammer: Add import author Model

--- backend/core/serializers/author.py
+++ backend/core/serializers/author.py
@@ -1,5 +1,7 @@
 from rest_framework_mongoengine import serializers
 
+from backend.core.models import Author
+
 
 class AuthorSerializer(serializers.EmbeddedDocumentSerializer):
     ref_name = "AuthorEvent"
