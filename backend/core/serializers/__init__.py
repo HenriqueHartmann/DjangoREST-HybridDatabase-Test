7622ad391f8d1b86459bc80c9ea9b8f3f5fb9102
Hartmann <alemaohartmann@gmail.com>
2020-09-22 15:48:05 -0300

    :mushroom: Add author e submission

--- backend/core/serializers/__init__.py
+++ backend/core/serializers/__init__.py
@@ -1,2 +1,4 @@
 from backend.core.serializers.user import UserSerializer
+from backend.core.serializers.author import AuthorSerializer
 from backend.core.serializers.event import EventSerializer
+from backend.core.serializers.submission import SubmissionSerializer
