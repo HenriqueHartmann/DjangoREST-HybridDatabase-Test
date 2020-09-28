8849bcb6dcf7f58af0341ffef270de8f1d8da902
Hartmann <alemaohartmann@gmail.com>
2020-09-28 14:06:36 -0300

    :alien: Alterar nome rota user

--- backend/core/router.py
+++ backend/core/router.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from backend.core import views
 
 router = routers.DefaultRouter()
-router.register("users", views.UserViewSet, basename="user")
+router.register("user", views.UserViewSet, basename="user")
 router.register("event", views.EventViewSet, basename="event")
 router.register("submission", views.SubmissionViewSet, basename="submission")
