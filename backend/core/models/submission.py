ce62d194906e0c6f844fd9f3e9b47e08b723a721
Hartmann <alemaohartmann@gmail.com>
2020-09-29 15:31:07 -0300

    :performing_arts: Add field event_ref e add required=True para file, event_ref e authors

--- backend/core/models/submission.py
+++ backend/core/models/submission.py
@@ -7,7 +7,8 @@ from backend.core.models import Author
 class Submission(Document):
     title = fields.StringField(max_length=50)
     title_english = fields.StringField(max_length=50)
-    file = fields.FileField()
+    file = fields.FileField(required=True)
     starts_on = fields.StringField(max_length=19)
     ends_on = fields.StringField(max_length=19)
-    authors = fields.EmbeddedDocumentListField(Author)
+    event_ref = fields.StringField(required=True)
+    authors = fields.EmbeddedDocumentListField(Author, required=True)
