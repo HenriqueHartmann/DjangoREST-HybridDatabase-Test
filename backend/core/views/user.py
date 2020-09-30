1ca769111f59bd2606217f33e5346265f21b6c69
Hartmann <alemaohartmann@gmail.com>
2020-09-30 17:58:04 -0300

    :cyclone: Add auto datatime no campo created_at em user

--- backend/core/views/user.py
+++ backend/core/views/user.py
@@ -1,4 +1,6 @@
 from rest_framework import status, viewsets
+from rest_framework.response import Response
+from datetime import datetime
 
 from backend.core import models
 from backend.core import serializers
@@ -7,3 +9,12 @@ from backend.core import serializers
 class UserViewSet(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
+
+    def create(self, request):
+        date = datetime.now()
+        request.data["created_at"] = date.strftime("%d-%m-%YT%H:%M:%S")
+        serializer = serializers.UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
