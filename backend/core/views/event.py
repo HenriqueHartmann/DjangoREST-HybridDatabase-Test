af55e1ed1d2e8de9a06c774acf98390ee5845ba2
Hartmann <alemaohartmann@gmail.com>
2020-09-30 21:57:44 -0300

    :snowflake: Add auto datatime ao campo created_at em event

--- backend/core/views/event.py
+++ backend/core/views/event.py
@@ -1,4 +1,7 @@
 from rest_framework_mongoengine import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from datetime import datetime
 
 from backend.core.models import Event
 from backend.core.serializers import EventSerializer
@@ -7,3 +10,12 @@ from backend.core.serializers import EventSerializer
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    def create(self, request):
+        date = datetime.now()
+        request.data["created_at"] = date.strftime("%d-%m-%YT%H:%M:%S")
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
