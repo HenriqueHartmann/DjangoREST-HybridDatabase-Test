5f02e072323f436013b8ecc5f0fd249086bf757d
Hartmann <alemaohartmann@gmail.com>
2020-09-30 21:54:38 -0300

    :tired_face: Adicionar campo created_at e alterar campo authors(remover required=True)

--- backend/core/models/event.py
+++ backend/core/models/event.py
@@ -9,4 +9,5 @@ class Event(Document):
     description = fields.StringField(max_length=255)
     starts_on = fields.StringField(max_length=19)
     ends_on = fields.StringField(max_length=19)
-    authors = fields.EmbeddedDocumentListField(Author, required=True)
+    created_at = fields.StringField(max_length=19)
+    authors = fields.EmbeddedDocumentListField(Author)
