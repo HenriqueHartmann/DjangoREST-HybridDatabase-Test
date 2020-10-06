4fdd1f9671090ac9afdd47b96eb98005ce4caa76
Hartmann <alemaohartmann@gmail.com>
2020-10-06 17:10:16 -0300

    :lock::sparkles: Add password field protection Serializer e add ListUsersSerializer

--- backend/core/serializers/tools.py
+++ backend/core/serializers/tools.py
@@ -3,6 +3,21 @@ from rest_framework import serializers
 from backend.core.serializers import UserSerializer, EventSerializer
 
 
+class FieldProfileShowSerializer(serializers.Serializer):
+    first_name = serializers.CharField(max_length=25)
+    last_name = serializers.CharField(max_length=50)
+
+
+class UserShowSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    profile = FieldProfileShowSerializer()
+    email = serializers.EmailField()
+    created_at = serializers.CharField()
+
 class ListEventsSerializer(serializers.Serializer):
-    user = UserSerializer()
+    user = UserShowSerializer()
     events = EventSerializer(required=False, many=True)
+
+class ListUsersSerializer(serializers.Serializer):
+    event = EventSerializer()
+    users = UserShowSerializer(many=True)
