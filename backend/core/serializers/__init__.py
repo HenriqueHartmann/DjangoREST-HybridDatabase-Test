584e9378d67c2d0fa5b35d37b8f63e5bb0ee7040
Hartmann <alemaohartmann@gmail.com>
2020-10-06 17:12:18 -0300

    :mushroom: Add UserShowSerializer e ListUsersSerializer

--- backend/core/serializers/__init__.py
+++ backend/core/serializers/__init__.py
@@ -2,4 +2,4 @@ from backend.core.serializers.user import UserSerializer
 from backend.core.serializers.author import AuthorSerializer
 from backend.core.serializers.event import EventSerializer
 from backend.core.serializers.submission import SubmissionSerializer
-from backend.core.serializers.tools import ListEventsSerializer
+from backend.core.serializers.tools import UserShowSerializer, ListEventsSerializer, ListUsersSerializer
