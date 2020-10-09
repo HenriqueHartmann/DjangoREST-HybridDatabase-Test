5adab4749b380b6dcaaac3268c689e5d4565e008
Hartmann <alemaohartmann@gmail.com>
2020-10-09 16:32:00 -0300

    :mushroom: Add user-record Serializer e ajustar import de tools.py

--- backend/core/serializers/__init__.py
+++ backend/core/serializers/__init__.py
@@ -1,5 +1,6 @@
 from backend.core.serializers.user import UserSerializer
+from backend.core.serializers.user_record import UserRecordSerializer
 from backend.core.serializers.author import AuthorSerializer
 from backend.core.serializers.event import EventSerializer
 from backend.core.serializers.submission import SubmissionSerializer
-from backend.core.serializers.tools import UserShowSerializer, ListEventsSerializer, ListUsersSerializer
+from backend.core.serializers.tools import *
