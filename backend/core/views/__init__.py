04e3e6c9324faa139370be47552f77a61514d043
Hartmann <alemaohartmann@gmail.com>
2020-10-09 16:33:42 -0300

    :mushroom: Add user-record

--- backend/core/views/__init__.py
+++ backend/core/views/__init__.py
@@ -1,3 +1,4 @@
 from backend.core.views.user import UserViewSet
+from backend.core.views.user_record import UserRecordViewSet
 from backend.core.views.event import EventViewSet
 from backend.core.views.submission import SubmissionViewSet
