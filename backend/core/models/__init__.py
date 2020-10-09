85b051d7b767200f8eabbf9ba3dc69cb882d60de
Hartmann <alemaohartmann@gmail.com>
2020-10-09 16:26:55 -0300

    :mushroom: Add DataUserRecord e UserRecord

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,4 +1,5 @@
 from backend.core.models.user import User
+from backend.core.models.user_record import DataUserRecord, UserRecord
 from backend.core.models.author import Author
 from backend.core.models.event import Event
 from backend.core.models.submission import Submission
