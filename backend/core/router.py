cbf2f8dd864805838b90178ab7934f07339f0f2b
Hartmann <alemaohartmann@gmail.com>
2020-10-09 16:43:48 -0300

    :articulated_lorry: Add rota user-record

--- backend/core/router.py
+++ backend/core/router.py
@@ -4,5 +4,6 @@ from backend.core import views
 
 router = routers.DefaultRouter()
 router.register("user", views.UserViewSet, basename="user")
+router.register("user-record", views.UserRecordViewSet, basename="user-record")
 router.register("event", views.EventViewSet, basename="event")
 router.register("submission", views.SubmissionViewSet, basename="submission")
