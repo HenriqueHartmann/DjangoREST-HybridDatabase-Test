e677dcb6eeb79d992ee546f485ad35192eea30ab
Hartmann <alemaohartmann@gmail.com>
2020-10-13 17:21:43 -0300

    :hammer: Refatorar fields e add field updated_at

--- backend/core/models/user_record.py
+++ backend/core/models/user_record.py
@@ -2,8 +2,9 @@ from mongoengine import Document, EmbeddedDocument, fields
 
 
 class DataUserRecord(EmbeddedDocument):
-    first_name: fields.StringField(max_length=25, required=True)
-    last_name: fields.StringField(max_length=50, required=True)
+    first_name = fields.StringField(max_length=25, required=True)
+    last_name = fields.StringField(max_length=50, required=True)
+    updated_at = fields.StringField(max_length=19)
 
 
 class UserRecord(Document):
