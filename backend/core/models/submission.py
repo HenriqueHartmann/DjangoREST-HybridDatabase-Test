2f9f068b0033feeebad29677e5180fb974ca3ce3
Hartmann <alemaohartmann@gmail.com>
2020-10-21 21:25:26 -0300

    :hammer: Reformular model submission

--- backend/core/models/submission.py
+++ backend/core/models/submission.py
@@ -1,6 +1,5 @@
 from mongoengine import Document, fields
 
-
 from backend.core.models import Author
 
 
@@ -8,8 +7,6 @@ class Submission(Document):
     title = fields.StringField(max_length=50)
     title_english = fields.StringField(max_length=50)
     file = fields.FileField(required=True)
-    starts_on = fields.StringField(max_length=19)
-    ends_on = fields.StringField(max_length=19)
     created_at = fields.StringField(max_length=19)
     event_ref = fields.StringField(required=True)
-    authors = fields.EmbeddedDocumentListField(Author)
+    authors = fields.ListField(required=True)
