47d0a01262b0be4e2bed4949b9c83f6942887c71
Hartmann <alemaohartmann@gmail.com>
2020-10-21 21:26:10 -0300

    :hammer: Reformular viewsets submission

--- backend/core/views/submission.py
+++ backend/core/views/submission.py
@@ -11,7 +11,6 @@ from backend.core import serializers
 
 
 class MultipartJsonParser(parsers.MultiPartParser):
-
     def parse(self, stream, media_type=None, parser_context=None):
         result = super().parse(
             stream,
@@ -20,29 +19,33 @@ class MultipartJsonParser(parsers.MultiPartParser):
         )
         data = {}
 
-        data = json.loads(result.data["data"])
-        
+        for key, value in result.data.items():
+            if type(value) != str:
+                data[key] = value
+                continue
+            if '{' in value or "[" in value:
+                try:
+                    data[key] = json.loads(value)
+                except ValueError:
+                    data[key] = value
+            else:
+                data[key] = value
+    
         qdict = QueryDict('', mutable=True)
         qdict.update(data)
         return parsers.DataAndFiles(qdict, result.files)
+
 
 class SubmissionViewSet(viewsets.ModelViewSet):
     queryset = models.Submission.objects.all()
     parser_classes = (MultipartJsonParser, parsers.JSONParser)
     serializer_class = serializers.SubmissionSerializer
-        
-    # Multipart field
+
     def create(self, request):
         date = datetime.now()
         request.data["created_at"] = date.strftime("%d-%m-%YT%H:%M:%S")
-        sr = serializers.Test(data=request.data["authors"])
-        if sr.is_valid():
-            print(sr.data)
-        else:
-            print(sr.errors)
-        print(request.data)
         serializer = serializers.SubmissionSerializer(data=request.data)
         if serializer.is_valid():
-            # serializer.save()
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
