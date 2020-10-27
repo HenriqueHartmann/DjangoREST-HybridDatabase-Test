933ce79a7ec468d98b351451235cfc94b2657997
Hartmann <alemaohartmann@gmail.com>
2020-10-26 21:57:11 -0300

    :turtle: Add GET(List e Retrieve) submission -> user

--- backend/core/views/submission.py
+++ backend/core/views/submission.py
@@ -1,7 +1,9 @@
 from django.http import QueryDict
+from django.shortcuts import get_object_or_404
 from rest_framework_mongoengine import viewsets
 from rest_framework import parsers, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.decorators import parser_classes
 from datetime import datetime
 import json
@@ -49,3 +51,41 @@ class SubmissionViewSet(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(methods=["GET"], detail=False, url_path="user")
+    def list_submissions_users(self, request):
+        submissions = models.Submission.objects.all()
+        users = models.User.objects.all()
+        list_all = []
+
+        if models.Submission.objects.count() > 0:
+            for submission in submissions:
+                list_temp = {}
+                list_temp["submission"] = submission
+                if models.User.objects.count() > 0:
+                    list_temp["users"] = []
+                    for user in users:
+                        list_ids = [int(a) for a in submission.authors]
+                        if user.id in list_ids:
+                            list_temp["users"].append(user)
+                list_all.append(list_temp)
+            serializer = serializers.ListUsersSubmissionsSerializer(list_all, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response([], status=status.HTTP_200_OK)
+
+    @action(methods=["GET"], detail=True, url_path="user")
+    def list_submission_users(self, request, id=None):
+        submissions = models.Submission.objects.all()
+        submission = get_object_or_404(submissions, id=id)
+        users = models.User.objects.all()
+        list_submission_users = {}
+        list_submission_users["submission"] = submission
+
+        if models.User.objects.count() > 0:
+            list_submission_users["users"] = []
+            for user in users:
+                list_ids = [int(a) for a in submission.authors]
+                if user.id in list_ids:
+                    list_submission_users["users"].append(user)
+        serializer = serializers.ListUsersSubmissionsSerializer(list_submission_users)
+        return Response(serializer.data, status=status.HTTP_200_OK)
